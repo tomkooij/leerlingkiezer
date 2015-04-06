44432d97808d1cb97f8349afd86e3537d4cf5792
tomkooij <tomkooij@tomkooij.nl>
2015-04-06 08:30:24 +0200

    dicts voor meer klassen

--- leerling_kiezer.py
+++ leerling_kiezer.py
@@ -2,7 +2,10 @@
 import pandas as pd
 import random
 
-FILENAME = 'export-60697.csv'
+# klassen bijbehorende SOMtoday CSV export
+csvfiles = { '1b' : 'export-60697.csv', '3e': 'export-60697.csv' }
+namen = {}
+
 def lees_namen_uit_csv(filename):
     """ Lees de leerlingnamen via Pandas dataframe
     return lijst van namen
@@ -19,11 +22,8 @@ def trek_naam(lijst):
 
 if  __name__ == '__main__':
 
-    if 'namen' not in globals():
-        print "Laad namen uit: ", FILENAME
-        global namen
-        namen = lees_namen_uit_csv(FILENAME)
-    else:
-        print "Namen reeds geladen..."
+    for klas in csvfiles:
+        print "klas %s uit %s " % (klas, csvfiles[klas])
+        namen[klas] = lees_namen_uit_csv(csvfiles[klas])
 
-    print trek_naam(namen)
+    #print trek_naam(namen)
