0ffdefcab34c6c5b136ed15cf397d68eba63d9fa
tomkooij <tomkooij@tomkooij.nl>
2015-04-15 14:07:52 +0200

    dropped unicode

--- leerling_kiezer.py
+++ leerling_kiezer.py
@@ -1,9 +1,10 @@
 # kies een random leerling uit een SOMtoday klassenexport
+# Somtoday gebruik CSV met ; als scheidingsteken en UTF-8 unicode
 import pandas as pd
 import random
 
 # klassen bijbehorende SOMtoday CSV export
-csvfiles = { '1b' : 'export-60697.csv', '3e': 'export-60697.csv' }
+csvfiles = { '1b' : '1b.csv', '3e': '3e.csv', '5na1': '5na1.csv' }
 namen = {}
 
 def lees_namen_uit_csv(filename):
@@ -11,13 +12,22 @@ def lees_namen_uit_csv(filename):
     return lijst van namen
     """
 
-    df = pd.read_csv(filename, header = 0, delimiter=';')
+    df = pd.read_csv(filename, header = 0, delimiter=';') # ,encoding='utf-8'
 
     return list(df['Naam'])
 
 def trek_naam(lijst):
     """ kies een random naam uit een een lijst """
     return lijst[random.randint(0,len(lijst)-1)]
+
+def maak_groepjes(lijst, n):
+    """ maak RANDOM groepjes van n leerlingen
+    return list of lists """
+
+    random.shuffle(lijst)
+
+    return [lijst[x:x+n] for x in range(0,len(lijst),n)]
+
 
 
 if  __name__ == '__main__':
@@ -26,4 +36,14 @@ if  __name__ == '__main__':
         print "klas %s uit %s " % (klas, csvfiles[klas])
         namen[klas] = lees_namen_uit_csv(csvfiles[klas])
 
-    #print trek_naam(namen)
+    klas1b = namen['1b']
+    klas3e = namen['3e']
+    klas5na1 = namen['5na1']
+    """
+    print "DEMO: willekeurige leerling uit 1b:", trek_naam(klas1b)
+
+    groepjes1b = maak_groepjes(klas1b,3)
+
+    for groep in groepjes1b:
+        print "DEMO: groepje: ", groep
+    """
